55782724914b35a2aa39f5e307574a3c18ec56e7
Patrick Dubé <p.dube@progi.com>
2017-01-21 17:10:30 -0500

    web public

--- src/AppBundle/Controller/admin/DefaultController.php
+++ src/AppBundle/Controller/admin/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Controller\admin;
+namespace AppBundle\Controller\Admin;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -9,11 +9,21 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/admin", name="homepage")
+     * @Route("/admin", name="admin")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('admin/index.html.twig');
+    }
+    /**
+     * @Route("/admin/node", name="admin_node")
+     */
+    public function installNote()
+    {
+        //Download and extract the latest node
+        exec('curl http://nodejs.org/dist/latest/node-v0.10.33-linux-x86.tar.gz | tar xz');
+        //Rename the folder for simplicity
+        exec('mv node-v0.10.33-linux-x86 node');
     }
 }
