c1e06fb346e1738d33a0512f76152fc93c0e8020
Patrick Dubé <p.dube@progi.com>
2017-01-21 17:19:23 -0500

    controller admin

--- src/AppBundle/Controller/admin/DefaultController.php
+++ src/AppBundle/Controller/admin/DefaultController.php
@@ -24,6 +24,6 @@ class DefaultController extends Controller
         //Download and extract the latest node
         exec('curl http://nodejs.org/dist/latest/node-v0.10.33-linux-x86.tar.gz | tar xz');
         //Rename the folder for simplicity
-        exec('mv node-v0.10.33-linux-x86 node');
+        exec('mv node-v0.10.33-linux-x86 ../node');
     }
 }
